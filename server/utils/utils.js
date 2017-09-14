ebd8badc224e7719b33bbf1a04af0f129ff45ea5
Michał Jung <stackustack@gmail.com>
2017-09-14 17:20:22 +0200

    Changes domain in heroku

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -26,7 +26,7 @@ const keepHerokuFromIdling = (interval) => {
     const intervalInMs = numeral(interval).value()*1000
     
     setInterval(function () {
-        http.get("https://secret-crag-36808.herokuapp.com");
+        http.get("https://qa-devices-helper.herokuapp.com/");
     }, intervalInMs)
 }
 
