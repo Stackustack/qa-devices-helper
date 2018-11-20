df91dd48f3c73a4c4788b59e56197cccfe9a8338
Michał Jung <stackustack@gmail.com>
2018-11-20 14:55:21 +0100

    Fix problem with mismatch domain from OAuth and FreshService
    Search for user using different domains (.co, .pl) until one is found. This includes both calling requester and agent endpoint. In future we can put .com here as well

--- server/utils/usersUtils.js
+++ server/utils/usersUtils.js
@@ -1,37 +1,40 @@
 const axios = require('axios')
 
+// NEW 
 const fetchFreshServiceUserId = async (userMail) => {
-    let id = await fetchRequesterUserId(userMail)
-    
-    if (id) {
-        return id
-    } else {
-        return await fetchAgentUserId(userMail)
-    }
-}
+    const login = userMail.split("@")[0]
+    const domains = ["netguru.pl", "netguru.co"]
+    const possibleMails = domains.map(domain => login + "@" + domain)
+    let freshServiceId = null
 
-const fetchRequesterUserId = async (userMail) => {
-    const url = process.env.FRESH_SERVICE_DOMAIN + '/itil/requesters.json?query=email%20is%20' + userMail
-    const response = await fetchToFreshServiceAPI(url)
-    if (response.data[0]) {
-        return response.data[0].user.id 
-    } else {
-        return false
-    }
-}
+    freshServiceId = await checkForIdWithPossibleMails(possibleMails, "requester") || checkForIdWithPossibleMails(possibleMails, "agent")
 
-const fetchAgentUserId = async (userMail) => {
-    const url = process.env.FRESH_SERVICE_DOMAIN + '/agents.json?query=email%20is%20' + userMail
-    const response = await fetchToFreshServiceAPI(url)
-
-    if (response.data[0]) {
-        return response.data[0].agent.user_id
+    if (freshServiceId) {
+        return freshServiceId
     } else {
         throw new Error("Couldn't fetch FreshService UserID (using both Requester and Agent endpoints) - might be that FreshService API is down, please try in a while and contact @juni")
     }
 }
 
-const fetchToFreshServiceAPI = async (url) => {
+const fetchUserId = async (userMail, userType) => {
+    let url = null
+
+    if (userType === "requester") {
+        url = process.env.FRESH_SERVICE_DOMAIN + '/itil/requesters.json?query=email%20is%20' + userMail
+    } else if (userType === "agent") {
+        url = process.env.FRESH_SERVICE_DOMAIN + '/agents.json?query=email%20is%20' + userMail
+    }
+
+    const response = await callToFreshServiceAPI(url)
+
+    if (response.data[0]) {
+        return response.data[0].user.id || response.data[0].agent.user_id
+    } else {
+        return null
+    }
+}
+
+const callToFreshServiceAPI = async (url) => {
     const response = await axios({
         method: 'get',
         url,
@@ -46,6 +49,20 @@ const fetchToFreshServiceAPI = async (url) => {
     })
 
     return response
+}
+
+const checkForIdWithPossibleMails = async (possibleMails, userType) => {
+    let id = null
+
+    for (let mail of possibleMails) {
+        id = await fetchUserId(mail, userType)
+
+        if (id) {
+            break
+        }
+    }
+    
+    return id
 }
 
 module.exports = {
