7fd1d3988afc37595294c80d24a188d4c0a43631
Michał Jung <stackustack@gmail.com>
2018-12-05 16:51:24 +0100

    Add util for slack integration

---
+++ server/utils/slackIntegrationUtils.js
@@ -0,0 +1,42 @@
+const axios = require('axios')
+
+const notifySupport = (errorData) => {
+	axios({
+		method: 'post',
+		url: process.env.SLACK_WEBHOOK_URI,
+		headers: { 'Content-Type': 'application/json' },
+		data: createSlackMessage(errorData)
+	})
+}
+
+
+const createSlackMessage = (errorData) => {
+	return {
+		"attachments": [
+			{
+				"title": ":pepe_scream: New problem related to QA Device Helper :pepe_scream:",
+				"fields": [
+					{
+						"title": "Error",
+						"value": errorData.shortMessage,
+						"short": true
+					},
+					{
+						"title": "User",
+						"value": errorData.userMail,
+						"short": true
+					}, {
+						"title": "Details",
+						"value": errorData.longMessage,
+						"short": false
+					}
+				],
+				"color": "#FA224D"
+			}
+		]
+	}
+}
+
+module.exports = {
+	notifySupport
+}
